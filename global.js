d48c63302722ff11db8bec3e23f767b8c6d68ab6
isaiahfang <ifang@ucsd.edu>
2025-01-24 14:49:55 -0800

    Save preference

--- global.js
+++ global.js
@@ -59,7 +59,17 @@ document.body.insertAdjacentHTML(
 
 let select = document.querySelector('.color-scheme select');
 
+document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
     document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+    localStorage.colorScheme = event.target.value
 });
+
+if ("colorScheme" in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+}
+
+select.value = localStorage.colorScheme;

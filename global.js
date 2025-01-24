4ca27eba2243168af7da6d186a9d18cc505bd448
isaiahfang <ifang@ucsd.edu>
2025-01-23 19:19:14 -0800

    Update global.js

--- global.js
+++ global.js
@@ -26,5 +26,22 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
 }
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+);
+
+a.classList.toggleAttribute(
+    '_blank',
+    a.host !== location.host
+);

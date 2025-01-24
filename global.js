8f0d2059dbeb326013a85557d25c8094ab857033
isaiahfang <ifang@ucsd.edu>
2025-01-23 20:49:19 -0800

    Navigation

--- global.js
+++ global.js
@@ -3,3 +3,37 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/isaiahfang', title: 'Github' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+}
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+);
+
+a.target.toggleAttribute(
+    '_blank',
+    a.host !== location.host
+);

f18708248edd1b7ac6b0fe5f7416937169203cbe
isaiahfang <ifang@ucsd.edu>
2025-01-29 18:54:55 -0800

    Update

--- projects/projects.js
+++ projects/projects.js
@@ -2,3 +2,6 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
+const count = document.querySelector('.projects-title');
+count.textContent = projects.length + " Projects";

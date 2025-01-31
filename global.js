29801946a534460656b9767746526a4ee5f7cef4
isaiahfang <ifang@ucsd.edu>
2025-01-30 16:20:50 -0800

    Fixed json errors

--- global.js
+++ global.js
@@ -88,13 +88,15 @@ export async function fetchJSON(url) {
 
 export function renderProjects(project, containerElement, headingLevel = 'h2') {
     containerElement.innerHTML = '';
-    const article = document.createElement('article');
-    article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-    `;
-    containerElement.appendChild(article);
+    for (let p of project) {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <h3>${p.title}</h3>
+        <img src="${p.image}" alt="${p.title}">
+        <p>${p.description}</p>
+        `;
+        containerElement.appendChild(article);
+    }
 }
 
 export async function fetchGitHubData(username) {

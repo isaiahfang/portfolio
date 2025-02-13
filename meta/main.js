c317830d72f318b2ed4d6e738c3cf080513adb59
isaiahfang <ifang@ucsd.edu>
2025-02-13 10:11:25 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -9,6 +9,8 @@ async function loadData() {
         date: new Date(row.date + 'T00:00' + row.timezone),
         datetime: new Date(row.datetime),
     }));
+
+    displayStats();
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -21,15 +23,55 @@ function processCommits() {
     commits = d3
         .groups(data, (d) => d.commit)
         .map(([commit, lines]) => {
-            // Each 'lines' array contains all lines modified in this commit
-            // All lines in a commit have the same author, date, etc.
-            // So we can get this information from the first line
             let first = lines[0];
-  
-            // What information should we return about this commit?
-            return {
+            let { author, date, time, timezone, datetime } = first;
+            let ret = {
                 id: commit,
-                // ... what else?
+                url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length,
             };
+  
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                // What other options do we need to set?
+                // Hint: look up configurable, writable, and enumerable
+                writable: true,
+                enumerable: false,
+                configurable: true,
+            });
+  
+            return ret;
         });
+}
+
+function displayStats() {
+    // Process commits first
+    processCommits();
+
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+
+    // Add more stats as needed...
+    dl.append('dt').text('Number of Files');
+    dl.append('dd').text(d3.groups(data, (d) => d.file).length);
+
+    dl.append('dt').text('Maximum File Length');
+    dl.append('dd').text(d3.max(data, (d) => d.length));
+
+    dl.append('dt').text('Longest File');
+    dl.append('dd').text(d3.max(data, (d) => d.file));
 }
